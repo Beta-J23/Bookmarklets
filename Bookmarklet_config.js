ad1509da9ee3cb8b98c0dfe7f286d8b66a79db73
Beta-J23 <76958823+Beta-J23@users.noreply.github.com>
2026-02-19 09:42:03 +0100

    Update and rename Bookmarklet_config.js.txt to Bookmarklet_config.js

---
+++ Bookmarklet_config.js
@@ -0,0 +1,4 @@
+window.bookmarkletMenuItems = [
+  { name: "Discogs List to Text", url: "https://raw.githubusercontent.com/Beta-J23/Bookmarklets/refs/heads/main/Discogs_list_text_in_new_window.txt" },
+  { name: "Dark Mode", url: "https://raw.githubusercontent.com/Beta-J23/Bookmarklets/refs/heads/main/Dark%20mode.txt" }
+];
