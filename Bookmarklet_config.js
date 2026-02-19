25c3e1d8aeae96a317e9cac0306c74a717f63dab
Beta-J23 <76958823+Beta-J23@users.noreply.github.com>
2026-02-19 10:57:42 +0100

    Update Bookmarklet_config.js

--- Bookmarklet_config.js
+++ Bookmarklet_config.js
@@ -1,7 +1,7 @@
 window.bookmarkletMenuItems = [
   { name: "Midi Monitor", url: "https://raw.githubusercontent.com/Beta-J23/Bookmarklets/refs/heads/main/Midi_Monitor.txt" },
   { name: "Discogs Tools", url: "" },
-  { name: "Discogs List to Text", url: "https://raw.githubusercontent.com/Beta-J23/Bookmarklets/refs/heads/main/Discogs_list_text_in_new_window.txt?token=GHSAT0AAAAAADUHUXCPOYX4VD3ZLXMM2UL62MWYS2Q" },
+  { name: "Discogs List to Text", url: "https://raw.githubusercontent.com/Beta-J23/Bookmarklets/refs/heads/main/Discogs_list_text_in_new_window.txt" },
   { name: "Discogs List to Youtube Playlist", url: "https://raw.githubusercontent.com/Beta-J23/Bookmarklets/refs/heads/main/Youtube_playlist_creator_from_discogs_list.txt" },
   { name: "Parsing Utils", url: "" },
   { name: "Json Parser", url: "https://raw.githubusercontent.com/Beta-J23/Bookmarklets/refs/heads/main/Json_parser.txt" },
