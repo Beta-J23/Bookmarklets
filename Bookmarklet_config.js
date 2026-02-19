09a12fd8c60e6ea100564d65282ff4a917d0fa32
Beta-J23 <76958823+Beta-J23@users.noreply.github.com>
2026-02-19 10:29:46 +0100

    Update Bookmarklet_config.js

--- Bookmarklet_config.js
+++ Bookmarklet_config.js
@@ -1,10 +1,10 @@
 window.bookmarkletMenuItems = [
-  { name: "Midi Monitor", url: "https://raw.githubusercontent.com/Beta-J23/Bookmarklets/refs/heads/main/Midi_Monitor.txt" }
-  { name: "Discogs Tools", url: "" }
+  { name: "Midi Monitor", url: "https://raw.githubusercontent.com/Beta-J23/Bookmarklets/refs/heads/main/Midi_Monitor.txt" },
+  { name: "Discogs Tools", url: "" },
   { name: "Discogs List to Text", url: "https://raw.githubusercontent.com/Beta-J23/Bookmarklets/refs/heads/main/Discogs_list_text_in_new_window.txt?token=GHSAT0AAAAAADUHUXCPOYX4VD3ZLXMM2UL62MWYS2Q" },
-  { name: "Discogs List to Youtube Playlist", url: "https://raw.githubusercontent.com/Beta-J23/Bookmarklets/refs/heads/main/Youtube_playlist_creator_from_discogs_list.txt" }
-  { name: "Parsing Utils", url: "" }
-  { name: "Json Parser", url: "https://raw.githubusercontent.com/Beta-J23/Bookmarklets/refs/heads/main/Json_parser.txt" }
-  { name: "Aspect", url: "" }
-  { name: "Dark Mode", url: "https://raw.githubusercontent.com/Beta-J23/Bookmarklets/refs/heads/main/Dark%20mode.txt?token=GHSAT0AAAAAADUHUXCPRVWNNP3OHHDVO2BC2MWYWWA" }
+  { name: "Discogs List to Youtube Playlist", url: "https://raw.githubusercontent.com/Beta-J23/Bookmarklets/refs/heads/main/Youtube_playlist_creator_from_discogs_list.txt" },
+  { name: "Parsing Utils", url: "" },
+  { name: "Json Parser", url: "https://raw.githubusercontent.com/Beta-J23/Bookmarklets/refs/heads/main/Json_parser.txt" },
+  { name: "Aspect", url: "" },
+  { name: "Dark Mode", url: "https://raw.githubusercontent.com/Beta-J23/Bookmarklets/refs/heads/main/Dark%20mode.txt?token=GHSAT0AAAAAADUHUXCPRVWNNP3OHHDVO2BC2MWYWWA" },
 ];
